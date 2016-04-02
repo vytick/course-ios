03f3e86869d142cf56264278d1528b4f03043769
vytick <vytick@gmail.com>
2016-04-03 00:50:01 +0200

    annotation

---
+++ course_annotation.swift
@@ -0,0 +1,52 @@
+
+
+/*
+    Kurz programování pro iOS vás naučí základní principy tvorby aplikací pro iOS,
+    představí vám programovací jazyk Swift a vlastní aplikaci si spolu také vytvoříme.
+*/
+
+class iOS {
+    
+    
+    var kdy = "5 pondělků 18-21 počínaje 18.4.2016"
+    var kde = "Paralelní Polis, Praha"
+    var kolikToStojí = "cena za celý kurz je   XXXX,- Kč"
+    
+    var coKTomuPotřebuju = "Vlastní MacBook a základní znalosti programování v libovolném jazyce"
+    
+    var sKým = "kurz povede Martin Vytrhlík"
+    
+    // konkrétní obsah jednotlivých lekcí přizpůsobíme tempu a může se stát,
+    // že toho zvládneme mnohem více. Tady je základní přehled toho, co bychom měli zvládnout
+    
+    func prvníLekce() {
+        // 1.
+        "Na první lekci se podíváme na zoubek jazyku Swift, ve kterém budeme později aplikaci vytvářet"
+        "Proletíme objektové programování, to jak s ním Swift pracuje a některé speciality jazyka."
+    }
+    
+    func druháLekce() {
+        // 2.
+        "Druhou lekci věnujeme základním stavebním kamenům iOS aplikace"
+        "Vyzkoušíme vytvořit jednoduchou aplikaci."
+    }
+    
+    func třetíLekce() {
+        // 3.
+        "Povíme si o Delegátech a Protokolech, k čemu nám jsou a jak se používají."
+        "Probereme nejpoužívanější stavební kámen aplikací - TableView a začneme budovat opravdovou aplikaci."
+    }
+    
+    func čtvrtáLekce() {
+        // 4.
+        "Řekneme si jak ukládat data a jak je pak zase z úložiště dostat."
+        "Budeme pracovat s polohou uživatelů, jazykem jejich operačního systému, možná přistupovat k fotografiím."
+    }
+    
+    func pátáLekce() {
+        // 5.
+        "Naučíme se zapojit knihovny jiných vývojářů a dotáhneme naši aplikaci k dokonalosti."
+        "Nakousneme (jako jablko), co udělat abychom aplikaci dostali až do appstore, k lidem."
+    }
+    
+}
